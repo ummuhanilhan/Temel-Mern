9fc3d324b16d502a60278c4774426dcb899a5264
ummuhanilhan <ummuhan.ilhan@bil.omu.edu.tr>
2022-10-04 11:10:34 +0300

    notlara erişme işlemi

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,31 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
-
+import { useEffect, useState } from 'react'
+import '../index.css'
 export default function Home() {
+const [notlar,setNotlar]=useState(null)
+useEffect(()=>{
+  const fetchNotlar=async ()=>{
+    const response=await fetch('/api/notlar')
+   
+    const json=await response.json()
+    console.log(json);
+    if(response.ok){
+      setNotlar(json)
+    }
+  }
+  
+fetchNotlar()
+
+},[])
   return (
     <div className="home">
-        <h2>Anasayfa</h2>
+        <div className='notlar'>
+        {notlar && notlar.map((not)=>(
+          <p key={not._id}>{not.baslik}</p>
+        )) }
+
+        </div>
     </div>
   )
 }
