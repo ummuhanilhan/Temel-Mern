b706da419f68e2f13e21366a550a8302088175df
ummuhanilhan <ummuhan.ilhan@bil.omu.edu.tr>
2022-10-04 10:12:06 +0300

    frontend ve backend eklendi

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+export default function Navbar() {
+  return (
+    <header>
+        <div className='container'>
+        <Link to="/">
+            <h1>Bilinova Not Defteri</h1>
+        </Link>
+        </div>
+    </header>
+  )
+}
