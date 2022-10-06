a584a07124d0cf4f1f02b7b112f16151b1e12054
ummuhanilhan <ummuhan.ilhan@bil.omu.edu.tr>
2022-10-06 11:44:25 +0300

    authcontext kullanımı

---
+++ frontend/src/contexts/authContext.js
@@ -0,0 +1,31 @@
+import { createContext,useReducer } from "react";
+
+
+export const AuthContext = createContext();
+
+export const authReducer=(state,action)=>{
+    switch(action.type){
+        case 'LOGIN':
+            return {
+                kullanici:action.payload
+            }
+        case 'LOGOUT':
+            return {
+                kullanici:null
+            }
+        default:
+            return state
+    }
+}
+
+export const AuthContextProvider=({children})=>{
+    const [state,dispatch]=useReducer(authReducer,
+        {kullanici:null})
+        console.log('AuthContext state:',state)
+        return(
+    <AuthContext.Provider value={{ ...state,dispatch }}>
+        {children}
+    </AuthContext.Provider>
+        )
+    }
+   
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { NotContextProvider } from './contexts/NotContext';
+import { AuthContextProvider } from './contexts/authContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthContextProvider>
    <NotContextProvider>
     <App />
    </NotContextProvider>
+   </AuthContextProvider>
   </React.StrictMode>
 );
