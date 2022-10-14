6d6ba3bf5f22544a033e2b18d34fcb00a821587f
ummuhanilhan <ummuhan.ilhan@bil.omu.edu.tr>
2022-10-14 15:11:21 +0300

    kullaniciya göre not gösterme işlemi

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,9 +1,12 @@
 import { useAuthContext } from "./useAuthContext";
+import { useNotContext } from "./useNotContext";
 
 export const useLogout = ()=>{
     const {dispatch}=useAuthContext()
+    const {dispatch: notDispatch}=useNotContext();
     const logout=()=>{
         localStorage.removeItem('kullanici')
+        notDispatch({type:'NOT_DOLDUR',payload:null})
         dispatch({type:'LOGOUT'})
     }
     return {logout}
