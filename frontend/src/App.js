a38fe3320fd87b12cc683ed47264c52c1207be23
ummuhanilhan <ummuhan.ilhan@bil.omu.edu.tr>
2022-10-14 14:51:24 +0300

    kullaniciya göre sayfa yönlendirme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 /* eslint-disable react/jsx-no-undef */
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import {useAuthContext} from './hooks/useAuthContext'
 function App() {
+const {kullanici}=useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar/>
         <div className='pages'>
         <Routes>
-        <Route path = '/' element = {<Home/>}/>
-        <Route path = '/signup' element = {<Signup/>}/>
-        <Route path = '/login' element = {<Login/>}/>
+        <Route path = '/' element = {kullanici ? <Home/> : <Navigate to="/login"/>}/>
+        <Route path = '/signup' element = {!kullanici ?<Signup/> : <Navigate to="/"/>}/>
+        <Route path = '/login' element = {!kullanici ? <Login/>: <Navigate to="/"/>}/>
         </Routes>
         </div>
       </BrowserRouter>
